b17de9b9b68fc8061df34e70522d1e1dfbe81d57
Hylmi Razzan <142045581+HylmiRazzan@users.noreply.github.com>
2026-02-26 21:35:47 +0700

    Update App.py

--- HeartAttackDeploy/App.py
+++ HeartAttackDeploy/App.py
@@ -5,11 +5,12 @@ import pandas as pd
 import os
 
 BASE_DIR = os.path.dirname(__file__)
-scaler = joblib.load(os.path.join(BASE_DIR, "preprocessor.pkl"))
+scaler = joblib.load(os.path.join(BASE_DIR, "preprocessing.pkl"))
 model = joblib.load(os.path.join(BASE_DIR, "model.pkl"))
 
 def main():
     st.title('Machine Learning Heart Attack Prediction Model Deployment')
 
     gender_options = {"Pria": 1, "Wanita": 0}
+
     gender = st.selectbox("Pilih Gender", options=list(gender_options.keys()))
