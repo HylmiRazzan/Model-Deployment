b71a98ab4e4f308f5620293511dc52305908f844
Hylmi Razzan <142045581+HylmiRazzan@users.noreply.github.com>
2026-03-02 22:45:28 +0700

    Update App.py

--- HeartAttack/App.py
+++ HeartAttack/App.py
@@ -20,7 +20,7 @@ if os.path.exists(img_path):
         .block-container {{
             margin-left: 0 !important;
             margin-right: auto !important;
-            padding-left: 5% !important;
+            padding-left: 20% !important;
             padding-top: 3% !important;
             padding-bottom: 100px !important;
             max-width: 500px !important;
@@ -91,5 +91,6 @@ def make_prediction(features):
     return prediction[0]
 if __name__ == '__main__':
     main()
+
 
 
